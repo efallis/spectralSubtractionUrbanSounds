a79c15ac99c6d4a518022946676b5a813666665a
efallis <efallis@uoguelph.ca>
2019-09-11 10:22:27 -0400

    Update urban_sound_filter_compare.m

--- urban_sound_filter_compare.m
+++ urban_sound_filter_compare.m
@@ -2,67 +2,115 @@ clc;
 clear;
 close all;
 
-% Add path to audio clips
+PATH_TO_FUNCTIONS = 'YOUR_PATH';
 PATH_TO_CLIPS = 'YOUR_PATH';
 
-cd (PATH_TO_CLIPS)
+addpath(PATH_TO_FUNCTIONS);
+cd (PATH_TO_CLIPS);
 
-SAMPLE_RATE = 44100;
+SAMPLE_RATE = 96000;
+FRAME_SIZE = 0.5;
+CLIP_SIZE = 10;
 
 % Pick your audio clips to be filtered
-name_audio = {'Human9';'Mechanical1';'Music17';'Nature1'};
+name_audio = {'Human_combo';'Mechanical_combo';'Music_combo';'Nature_combo'};
 name_algo = {'No Algorithm';'Boll';'Berouti';'Kamath'};
-accuracy = length(name_algo);
-quality= length(name_algo);
-algorithm_signals = cell(4,1);
-algorithm_noise = cell(4,1);
 
-for k = 1:length(name_audio)
-    figure;
-    disp(name_audio{k});
-    path_source = strcat(name_audio{k},'.wav');
+path_mic = 'mic.wav';
+[y_mic,Fs_mic] = audioread(path_mic);
+
+y_mic = y_mic(:,1);
+t_mic = (1:size(y_mic))/Fs_mic;
+
+y_mic = resample(y_mic,SAMPLE_RATE,Fs_mic);
+
+for i = 1:length(name_audio)
+    accuracy = zeros(length(name_algo),CLIP_SIZE);
+    accuracy_average = zeros(length(name_algo),1);
+    quality = zeros(length(name_algo),CLIP_SIZE);
+    quality_average = zeros(length(name_algo),1);
+    algorithm_signals = cell(length(name_algo),CLIP_SIZE);
+    algorithm_noise = cell(length(name_algo),CLIP_SIZE);
+
+    disp(name_audio{i});
+    path_source = strcat(name_audio{i},'.wav');
     [y_source,Fs_source] = audioread(path_source);
     
     % Only take real values
     y_source = y_source(:,1);
 
-    % Resample to 44100
+    % Resample to SAMPLE_RATE
     y_source = resample(y_source,SAMPLE_RATE,Fs_source);
     Fs_source = SAMPLE_RATE;
 
-    path_mic = 'all_noise_most.wav';
-    [y_mic,Fs_mic] = audioread(path_mic);
-
     y_source = y_source(:,1);
     t_source = (1:size(y_source))/Fs_source;
-    y_source_envelope = envelope(y_source, 100, 'peak');
-
-    y_mic = y_mic(:,1);
-    t_mic = (1:size(y_mic))/Fs_mic;
-
-    % No algorithm
-    algorithm_signals{1} = y_mic;
-
-    algorithm_signals{2} = SSBoll79(y_mic, Fs_mic);
-
-    algorithm_signals{3} = SSBerouti79(y_mic, Fs_mic);
-
-    algorithm_signals{4} = SSMultibandKamath02(y_mic, Fs_mic);
+    y_source_envelope = envelope(y_source, 150, 'peak');
 
     % Find the start of the recording
     mic_delay = finddelay(y_source, y_mic);
-    if (contains(name_audio{k},"Mech"))
-        mic_delay = mic_delay - 9500;
-    end
 
-    for i = 1:4
-        algorithm_noise{i} = mean(algorithm_signals{i}(round(SAMPLE_RATE*0.1):round(SAMPLE_RATE*0.5)).^2);
+    % Adjust the time frame to make it line up
+    y_mic_clip = y_mic(mic_delay+1:mic_delay + SAMPLE_RATE*40);
+    
+    % Loop through all slices in each clip (10)
+    for j = 1:CLIP_SIZE
+        y_mic_slice = y_mic_clip((j-1)*SAMPLE_RATE+1:(j+3)*SAMPLE_RATE);
         
-        % Adjust the time frame to make it line up
-        algorithm_signals{i} = algorithm_signals{i}(mic_delay+1:mic_delay + SAMPLE_RATE*4);
-        
-        if (contains(name_audio{k},"Human") || contains(name_audio{k},"Mech"))
-            algorithm_signals{i} = algorithm_signals{i}(1:SAMPLE_RATE*3.5);
+        % Put lowest noise part at the beginning
+        power_min = inf;
+
+        % Find the FRAME_SIZE s clip at which the power is the lowest
+        for k = 1:floor((length(y_mic_slice)/SAMPLE_RATE) / FRAME_SIZE)-1
+            power_signal = mean(abs(y_mic_slice(k*FRAME_SIZE*SAMPLE_RATE:(k+1)*FRAME_SIZE*SAMPLE_RATE)).^2);
+            if power_signal < power_min
+                power_min = power_signal;
+                power_min_index = k;
+            end
+        end
+
+        clip_start = power_min_index*FRAME_SIZE*SAMPLE_RATE;
+        clip_end = (power_min_index + 1)*FRAME_SIZE*SAMPLE_RATE;
+
+        power_min = y_mic_slice(clip_start:clip_end);
+
+        y_mic_slice(clip_start:clip_end) = [];
+
+        y_mic_slice = [power_min; y_mic_slice];
+
+
+        % No algorithm first
+        algorithm_signals{1}{j} = y_mic_slice;
+        algorithm_signals{2}{j} = SSBoll79(y_mic_slice, SAMPLE_RATE);
+        algorithm_signals{3}{j} = SSBerouti79(y_mic_slice, SAMPLE_RATE);
+        algorithm_signals{4}{j} = SSMultibandKamath02(y_mic_slice, SAMPLE_RATE);
+
+        for k = 1:length(name_algo)
+            algorithm_noise{k}{j} = mean(algorithm_signals{k}{j}(round(SAMPLE_RATE*0.1):round(SAMPLE_RATE*0.5)).^2);
+
+            % Reconstruct the signal (put the first part back where it belongs)
+            algorithm_signals{k}{j} = [
+                                        algorithm_signals{k}{j}(FRAME_SIZE*SAMPLE_RATE:min(FRAME_SIZE*SAMPLE_RATE+clip_start,end));
+                                        algorithm_signals{k}{j}(1:FRAME_SIZE*SAMPLE_RATE);
+                                        algorithm_signals{k}{j}(FRAME_SIZE*SAMPLE_RATE+clip_start:end)
+                                       ];
+
+            subplot(length(name_algo),1,k);
+
+            % Coherence
+            y_source_slice = y_source((j-1)*SAMPLE_RATE+1:(j-1)*SAMPLE_RATE+length(algorithm_signals{k}{j}));
+            
+            [coherence, f] = mscohere(algorithm_signals{k}{j}, y_source_slice, [], [], [], SAMPLE_RATE);
+            coherence_envelope = envelope(coherence, 100, 'peak');
+
+            accuracy(k,j) = mean(coherence_envelope);
+
+            power_signal = mean(algorithm_signals{k}{j}.^2);
+
+            power_signal = power_signal - algorithm_noise{k}{j};
+
+            quality(k,j) = power_signal/algorithm_noise{k}{j};
+            quality(k,j) = 10*log(quality(k,j));
         end
     end
 
@@ -71,41 +119,22 @@ for k = 1:length(name_audio)
                [0.9290 0.6940 0.1250];
                [0.4660 0.6740 0.1880]];
 
-    % Plot each algorithm
-    for i = 1:length(name_algo)
-        % Uncomment to play each clip before and after algorithm
-%         player = audioplayer(algorithm_signals{i},Fs_source);
-%         player.play;
-%         pause(4);
+    % Plot each category
+    subplot(length(name_audio),1,i);
 
-        subplot(length(name_algo),1,i);
+    plot(t_source,y_mic_clip);
+    hold on
+    plot(t_source,y_source_envelope);
+    hold off
+    set(gca, 'YLim',[-1 1]);
+    set(gca, 'XLim',[0 20]);
+    title(strrep(name_audio{i}, '_', ' '));
 
-        plot(t_source(1:length(algorithm_signals{i})),algorithm_signals{i});
-        hold on
-        plot(t_source,y_source_envelope);
-        hold off
-        set(gca, 'YLim',[-1 1]);
-        set(gca, 'XLim',[0 3.5]);
+    for j = 1:length(name_algo)
+        accuracy_average(j) = mean(accuracy(j,1:10));
+        quality_average(j) = mean(quality(j,1:10));
     end
-
-    % Coherence
-    for i = 1:length(name_algo)
-        subplot(length(name_algo),1,i);
-
-        [coherence, f] = mscohere(algorithm_signals{i}, y_source(1:length(algorithm_signals{i})), [], [], [], SAMPLE_RATE);
-        coherence_envelope = envelope(coherence, 100, 'peak');
-
-        accuracy(i) = mean(coherence_envelope);
-
-        power_signal = mean(algorithm_signals{i}.^2);
-       
-        power_signal = power_signal - algorithm_noise{i};
-
-        quality(i) = power_signal/algorithm_noise{i};
-        quality(i) = 10*log(quality(i));
-    end
-
-    t = table(name_algo,accuracy',quality');
+    t = table(name_algo,accuracy_average,quality_average);
     t.Properties.VariableNames = {'Algorithm','Accuracy','SNR'};
     disp(t)
 end
